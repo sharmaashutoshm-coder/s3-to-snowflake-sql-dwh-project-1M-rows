249494ddcdc9469ac4121f5e5a8e08be31b14e8b
Ashutosh Sharma <sharmaashutoshm@gmail.com>
2026-02-11 20:18:02 +0530

    Modify init_database.sql to recreate database and schemas
    Updated script to create 'SQL_DWH_PROJECT' database and schemas.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -5,8 +5,17 @@ Create Database and Schemas
 
 Script Purpose:
 
-    This script sets up three schemas
-    within the database: 'bronze', 'silver', and 'gold'.
+    This script creates a new database named 'SQL_DWH_PROJECT' after checking if it already exists.
+
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+    within the database: 'BRONZE', 'SILVER', and 'GOLD'.
+
+WARNING:
+
+    Running this script will drop the entire 'SQL_DWH_PROJECT' database if it exists.
+
+    All data in the database will be permanently deleted. Proceed with caution
+    and ensure you have proper backups before running this script.
 
 */
 
